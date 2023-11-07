a5247707d89e909c4f6c87ce0479117a832b4c8a
oubaidhl <hlaymiabid@gmail.com>
2023-11-08 00:11:47 +0100

    http: https

--- src/app/shared/Service/Stock.service.ts
+++ src/app/shared/Service/Stock.service.ts
@@ -7,7 +7,7 @@ import { HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class StockService {
-  readonly API_URL = 'https://192.168.1.160:8089/SpringMVC/stock';
+  readonly API_URL = 'http://192.168.1.160:8089/SpringMVC/stock';
 
   constructor(private httpClient: HttpClient) { }
 
